3082d2309f4f1eba72d6c5db7221946cd2bc2983
AbdallahDay <abdallah.day92@gmail.com>
2015-03-12 22:22:55 -0500

    some stuff

--- src/com/Group5/StockPile.java
+++ src/com/Group5/StockPile.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 /**
  * Created by Abdallah on 2/19/2015.
  */
-public class Pile {
+public class StockPile {
     public static LinkedList<Cards> stockPile = new LinkedList<Cards>();
 
     public static Cards Draw() {
