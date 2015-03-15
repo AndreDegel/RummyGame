41debea4e451ce21801c0de8bb6f85e79ebd218d
AbdallahDay <abdallah.day92@gmail.com>
2015-03-14 19:05:34 -0500

    worked more on Discard() method for PCPlayer

--- src/com/Group5/PCPlayer.java
+++ src/com/Group5/PCPlayer.java
@@ -2,6 +2,7 @@ package com.Group5;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * Player class that represents the Computer player (AI)
@@ -22,6 +23,53 @@ public class PCPlayer extends Player {
 
         //Draw
         Draw();
+
+        ArrayList<Cards> hand = playerHand.getAllCards();
+
+        ArrayList<Cards> spades = new ArrayList<Cards>();
+        ArrayList<Cards> clubs = new ArrayList<Cards>();
+        ArrayList<Cards> hearts = new ArrayList<Cards>();
+        ArrayList<Cards> diamonds = new ArrayList<Cards>();
+
+        //Separate hand cards by suit
+        for (Cards card : hand) {
+            Suit cardSuit = card.getSuit();
+
+            switch (cardSuit) {
+                case Spades:
+                    spades.add(card);
+                    break;
+                case Clubs:
+                    clubs.add(card);
+                    break;
+                case Hearts:
+                    hearts.add(card);
+                    break;
+                case Diamonds:
+                    diamonds.add(card);
+                    break;
+            }
+        }
+
+        //Sort each group of cards
+        if (!spades.isEmpty()) {
+            Collections.sort(spades);
+        }
+        if (!clubs.isEmpty()) {
+            Collections.sort(clubs);
+        }
+        if (!hearts.isEmpty()) {
+            Collections.sort(hearts);
+        }
+        if (!diamonds.isEmpty()) {
+            Collections.sort(diamonds);
+        }
+
+        HashMap<Suit, ArrayList<Cards>> suitGroups = new HashMap<Suit, ArrayList<Cards>>();
+        suitGroups.put(Suit.Spades, spades);
+        suitGroups.put(Suit.Clubs, clubs);
+        suitGroups.put(Suit.Hearts, hearts);
+        suitGroups.put(Suit.Diamonds, diamonds);
 
         //Meld if necessary
 
@@ -54,49 +102,49 @@ public class PCPlayer extends Player {
         }
     }
 
-    private void Discard() {
-        ArrayList<Cards> hand = playerHand.getAllCards();
-
-        ArrayList<Cards> spades = new ArrayList<Cards>();
-        ArrayList<Cards> clubs = new ArrayList<Cards>();
-        ArrayList<Cards> hearts = new ArrayList<Cards>();
-        ArrayList<Cards> diamonds = new ArrayList<Cards>();
-
+    private void Discard(HashMap<Suit, ArrayList<Cards>> suitGroups) {
+        ArrayList<Cards> discardable = new ArrayList<Cards>();
+        ArrayList<Cards> reserve = new ArrayList<Cards>();
 
         //TODO: figure out which card to discard
         //TODO: sort ArrayList so that least desired cards are at the front
-        for (Cards card : hand) {
-            //Separate hand cards by suit
-            Suit cardSuit = card.getSuit();
 
-            switch (cardSuit) {
-                case Spades:
-                    spades.add(card);
-                    break;
-                case Clubs:
-                    clubs.add(card);
-                    break;
-                case Hearts:
-                    hearts.add(card);
-                    break;
-                case Diamonds:
-                    diamonds.add(card);
-                    break;
+        //Measure difference between same-suit cards and act accordingly
+        for (Suit suit : suitGroups.keySet()) {
+            ArrayList<Cards> cards = suitGroups.get(suit);
+
+            HashMap<String, ArrayList<Cards>> subgroups = new HashMap<String, ArrayList<Cards>>();
+
+            int prevCardIndex = 0;                                      //start first card in group
+
+            for (int currCardIndex = 1;                                 //loop over the rest of the cards
+                 currCardIndex < cards.size();                          //in the group starting with
+                 currCardIndex++) {                                     //the second card
+
+                Cards prevCard = cards.get(prevCardIndex);
+                Cards currCard = cards.get(currCardIndex);
+
+                int prevCardRankValue = prevCard.getRank().getValue();
+                int currCardRankValue = currCard.getRank().getValue();
+
+                int rankDiff = currCardRankValue - prevCardRankValue;
+
+                if (rankDiff <= 2) {
+                    String subgroupName = null;
+                    for (String name : subgroups.keySet()) {
+                        ArrayList<Cards> subgroup = subgroups.get(name);
+                        if (subgroup.contains(prevCard)) {
+                            subgroupName = name;
+                        }
+                    }
+
+                    if (subgroupName == null) {
+
+                    }
+                }
+
+                prevCardIndex = currCardIndex;
             }
-        }
-
-        //Sort each group of cards
-        if (!spades.isEmpty()) {
-            Collections.sort(spades);
-        }
-        if (!clubs.isEmpty()) {
-            Collections.sort(clubs);
-        }
-        if (!hearts.isEmpty()) {
-            Collections.sort(hearts);
-        }
-        if (!diamonds.isEmpty()) {
-            Collections.sort(diamonds);
         }
     }
 
@@ -105,20 +153,32 @@ public class PCPlayer extends Player {
     }
 
     private boolean hasRank(Rank r) {
-        for (Cards c : playerHand.getAllCards()) {
+        boolean doesHaveRank = false;
+
+        int i = 0;
+        while (i < playerHand.getAllCards().size()) {
+            Cards c = playerHand.getCard(i);
             if (c.getRank() == r) {
-                return true;
+                doesHaveRank = true;
+                break;
             }
         }
-        return false;
+
+        return doesHaveRank;
     }
 
     private boolean hasSuit(Suit s) {
-        for (Cards c : playerHand.getAllCards()) {
+        boolean doesHaveSuit = false;
+
+        int i = 0;
+        while (i < playerHand.getAllCards().size()) {
+            Cards c = playerHand.getCard(i);
             if (c.getSuit() == s) {
-                return true;
+                doesHaveSuit = true;
+                break;
             }
         }
-        return false;
+
+        return doesHaveSuit;
     }
 }
